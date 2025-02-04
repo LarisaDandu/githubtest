c68a8de85e4c4c552afc7e77eee380c593b190d7
Larisa Maria Dandu <eaa24lamd@students.eaaa.dk>
2025-02-04 09:09:19 +0100

    mhm

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>*angel aura sounds*</h1>
+      <h1>*angle aura sounds*</h1>
       <img src = {golden}/>
       <h2>You have been blessed with swag.</h2>
       
